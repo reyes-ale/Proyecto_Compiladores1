c5ef53a2abe23511180c223876b73aa144fb9b22
Juanjo Moreira <147878947+jjlomo@users.noreply.github.com>
2026-09-21 23:59:29 -0600

    Inicio del parser

---
+++ Parser.h
@@ -0,0 +1,18 @@
+#include "Parser.h"
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "Token.h"
+using namespace std;
+
+class Parser {
+    private:
+        vector<Token> tokens;
+        int actual;
+
+    public:
+        Parser();
+        Parser(vector<Token> t);
+        int getActual();
+        
+};
